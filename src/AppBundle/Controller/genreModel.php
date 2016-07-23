282406fc757e95e5d26468f3cf56b96d2b440da0
lauti337r <lau337.r@gmail.com>
2016-07-23 20:13:42 -0300

    Arreglo conn_db

--- src/AppBundle/Controller/genreModel.php
+++ src/AppBundle/Controller/genreModel.php
@@ -16,12 +16,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class genreModel{
     function listMovByGenre($genre){
-        $db = new \mysqli("localhost","root","","w2w");
+        global $db;
+        //$db = new \mysqli("localhost","root","","w2w");
         return $db->query("SELECT m.id FROM movie AS m, genre AS g, moviegenre AS mg 
                             WHERE g.name=\"$genre\" AND m.id=mg.id_movie AND mg.id_genre=g.id");
     }
     function listTVByGenre($genre){
-        $db = new \mysqli("localhost","root","","w2w");
+        global $db;
+        //$db = new \mysqli("localhost","root","","w2w");
         return $db->query("SELECT t.id FROM tvshow AS t, genre AS g, tvshowgenre AS tg
                             WHERE g.name=\"$genre\" AND t.id=tg.id_tvshow AND tg.id_genre=g.id");
     }
