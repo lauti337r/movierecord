d6b9425935c58a01d0aa9989b35503c17626e397
lauti337r <lau337.r@gmail.com>
2016-07-23 20:13:10 -0300

    Arreglo conn_db

--- src/AppBundle/Controller/artistModel.php
+++ src/AppBundle/Controller/artistModel.php
@@ -11,11 +11,13 @@ namespace AppBundle\Controller;
 
 class artistModel{
     function idArt($artist){
-        $db = new \mysqli("localhost","root","","w2w");
+        global $db;
+        //$db = new \mysqli("localhost","root","","w2w");
         return $db->query("SELECT id FROM artist WHERE name=\"$artist\"")->fetch_row()[0];
     }
     function listMovApp($idArt){
-        $db = new \mysqli("localhost","root","","w2w");
+        global $db;
+        //$db = new \mysqli("localhost","root","","w2w");
         $res = $db->query("SELECT m.id,m.title,a.name,m.year,m.plot,m.review,m.runtime,m.releaseDate,m.poster 
                             FROM movie AS m, artist AS a, moviecast as mc 
                             WHERE mc.id_act=$idArt AND a.id=m.id_dir AND m.id=mc.id_movie");
@@ -23,14 +25,16 @@ class artistModel{
         else return -1;
     }
     function listMovDirected($idDir){
-        $db = new \mysqli("localhost","root","","w2w");
+        global $db;
+        //$db = new \mysqli("localhost","root","","w2w");
         $res = $db->query("SELECT m.id,m.title,a.name,m.year,m.plot,m.review,m.runtime,m.releaseDate,m.poster 
                           FROM movie AS m, artist AS a WHERE m.id_dir=$idDir AND m.id_dir=a.id");
         if($res->num_rows != 0)return $res;
         else return -1;
     }
     function listTVSApp($idArt){
-        $db = new \mysqli("localhost","root","","w2w");
+        global $db;
+        //$db = new \mysqli("localhost","root","","w2w");
         $res = $db->query("SELECT t.id,t.title,a.name,t.year_s,t.year_e,t.plot,t.review,t.poster 
                           FROM tvshow AS t, artist AS a, tvshowcast as tc
                           WHERE tc.id_act=$idArt AND a.id=tc.id_act AND t.id=tc.id_tvshow");
@@ -38,7 +42,8 @@ class artistModel{
         else return -1;
     }
     function listTVSWritten($idWri){
-        $db = new \mysqli("localhost","root","","w2w");
+        global $db;
+        //$db = new \mysqli("localhost","root","","w2w");
         $res = $db->query("SELECT t.id,t.title,a.name,t.year_s,t.year_e,t.plot,t.review,t.poster 
                           FROM tvshow AS t, artist AS a WHERE t.id_writer=$idWri AND t.id_writer=a.id");
         if($res->num_rows != 0)return $res;
